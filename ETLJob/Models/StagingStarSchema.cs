a4923af2a9a135e56cb6dc61eec8bf687e93b0ed
Wajeeh Ahmed <wajeehnaeem@hotmail.com>
2017-04-28 17:00:38 +0500

    Staging Star Schema Done

--- ETLJob/Models/StagingStarSchema.cs
+++ ETLJob/Models/StagingStarSchema.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace ETLJob.Models
 {
@@ -12,8 +13,81 @@ namespace ETLJob.Models
 
     }
 
-    public class Dates
+    public class Date
     {
-        public DateTime DatePK { get; set; }
+        [Key]
+        public DateTime date { get; set; }
+        public int TimeQuadrant { get; set; }
+        public String DateDescription { get; set; }
+        public string DayOfWeek { get; set; }
+        public System.Nullable<int> DayNumberInCalenderMonth { get; set; }
+        public System.Nullable<int> DayNumberInCalenderYear { get; set; }
+        public System.Nullable<int> LastDayInMonthIndicator { get; set; }
+        public System.Nullable<int> CalenderWeekNumberInYear { get; set; }
+        public String CalenderMonthName { get; set; }
+        public System.Nullable<int> CalenderMonthNumberInYear { get; set; }
+        public System.Nullable<DateTime> CalenderWeekEndingDate { get; set; }
+        public int CalenderQuarter { get; set; }
+        public System.Nullable<int> CalenderYear { get; set; }
+        public String Season { get; set; }
+        public String CalenderYearMonth { get; set; }
+        public System.Nullable<int> HolidayIndicator { get; set; }
+        public String HolidayDescriptor { get; set; }
+        public System.Nullable<DateTime> SqlDateTimestamp { get; set; }
+        public System.Nullable<DateTime> UnixDateTimestamp { get; set; }
+        public System.Nullable<int> WeekdayIndicator { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    public class Location
+    {
+        [Key]
+        public String LocationId { get; set; }
+        public string City { get; set; }
+        public String Country { get; set; }
+        public String Province { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    public class Product
+    {
+        [Key]
+        public String ProductId { get; set; }
+        //public String skuNumber { get; set; }
+        public String ProductDescription { get; set; }
+        public String BrandDescription { get; set; }
+        public String CategoryDescription { get; set; }
+        public String ProductSize { get; set; }
+        //public String ProductRelatedInfo { get; set; }
+
+        //public int itemPrice { get; set; } will get by drill through
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    public class Customer
+    {
+        [Key]
+        public String CustomerId { get; set; }
+        public String Name { get; set; }
+        //public String Age { get; set; }
+        public String City { get; set; }
+
+        //public String address { get; set; }
+        public String Gender { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public virtual Date DateFK { get; set; }
+        public virtual Product ProductFK { get; set; }
+        public virtual Location LocationFK { get; set; }
+        public virtual Customer CustomerFK { get; set; }
+        public int TransactionAmount { get; set; }
+        public int DiscountedAmount { get; set; }
+        public int NetAmount { get; set; }
     }
 }
---
+++ ETLJob/Models/StagingStarSchemaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ETLJob.Models
+{
+    class StagingStarSchemaContext : DbContext
+    {
+        public DbSet<Date> Dates { get; set; } 
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
