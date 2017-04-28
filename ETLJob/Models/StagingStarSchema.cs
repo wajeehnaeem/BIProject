3be0880c535531643a4884fa1e40a171a2bc1a00
Wajeeh Ahmed <wajeehnaeem@hotmail.com>
2017-04-28 16:45:31 +0500

    Added EF

---
+++ ETLJob/Models/StagingStarSchema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ETLJob.Models
+{
+    class StagingStarSchema
+    {
+
+    }
+
+    public class Dates
+    {
+        public DateTime DatePK { get; set; }
+    }
+}
